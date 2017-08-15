3cfbf5cf9896f0e404c2554f9beb0a3d61f3ad94
Git Faf <git.faf@gmail.com>
2017-08-15 23:55:00 +0530

    Random arithmetic expression generator for a number.

---
+++ problems/expression-generator/README.md
@@ -0,0 +1,19 @@
+# Expression generator
+
+### Random expression
+
+Generate expressions for a number randomly using +1, *2 and *3 as operators to a given value.
+
+```
+    console.log(findExpression(24));
+    ((((((((((((((1 * 2) + 1) + 1) + 1) * 3) + 1) + 1) + 1) + 1) + 1) + 1) + 1) + 1) + 1)
+
+    console.log(findExpression(24));
+    ((((((((1 * 3) + 1) + 1) * 2) + 1) * 2) + 1) + 1)
+
+    console.log(findExpression(24));
+    (((((((((((1 * 2) * 2) + 1) + 1) * 3) + 1) + 1) + 1) + 1) + 1) + 1)
+
+    console.log(findExpression(24));
+    ((((1 + 1) * 3) * 2) * 2)
+```
---
+++ problems/expression-generator/random-expression.js
@@ -0,0 +1,31 @@
+function generateRandomExpression (number) {
+	const steps = [
+		{ action: x => x * 3, update: history => `(${history} * 3)`},
+		{ action: x => x * 2, update: history => `(${history} * 2)`},
+		{ action: x => x + 1, update: history => `(${history} + 1)`}
+	];
+
+	function executeStepsAndPickOne (current, history) {
+		let all = [];
+		while(all.indexOf(0) < 0 || all.indexOf(1) < -1 || all.indexOf(2) < -1) {
+			let random = parseInt(Math.random() * 3);
+			let step = steps[random];
+			let value = nextStep(step.action(current), step.update(history));
+			if (value) {
+				all.push(random);
+				return value;
+			}
+		}
+	}
+	
+    function nextStep (current, history) {
+        if (current > number) {
+            return null;
+        }
+        if (current === number) {
+            return history;
+		}
+        return executeStepsAndPickOne(current, history);
+    }
+    return nextStep(1, `1`);
+}
