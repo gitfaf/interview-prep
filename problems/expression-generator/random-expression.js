767e72710d2f936923cc3d56d81230aa3ad7d376
Git Faf <git.faf@gmail.com>
2017-08-16 00:48:04 +0530

    Removing while loop to introduce do-while - [JS] - [expression-generator]

--- problems/expression-generator/random-expression.js
+++ problems/expression-generator/random-expression.js
@@ -1,30 +1,34 @@
-function generateRandomExpression (number) {
-	const steps = [
-		{ action: x => x * 3, update: history => `(${history} * 3)`},
-		{ action: x => x * 2, update: history => `(${history} * 2)`},
-		{ action: x => x + 1, update: history => `(${history} + 1)`}
-	];
+function generateRandomExpression(number) {
+    const steps = [{
+            action: x => x * 3,
+            update: history => `(${history} * 3)`
+        },
+        {
+            action: x => x * 2,
+            update: history => `(${history} * 2)`
+        },
+        {
+            action: x => x + 1,
+            update: history => `(${history} + 1)`
+        }
+    ];
 
-	function executeStepsRandomlyAndPickOne (current, history) {
-		let all = [];
-		while(all.indexOf(0) < 0 || all.indexOf(1) < -1 || all.indexOf(2) < -1) {
-			let random = parseInt(Math.random() * 3);
-			let step = steps[random];
-			let value = nextStep(step.action(current), step.update(history));
-			if (value) {
-				all.push(random);
-				return value;
-			}
-		}
-	}
+    function executeStepsRandomlyAndPickOne(current, history) {
+        let value = null;
+        do {
+            let step = steps[Math.floor(Math.random() * steps.length)];
+            value = nextStep(step.action(current), step.update(history));
+        } while (!value);
+        return value;
+    }
 
-    function nextStep (current, history) {
+    function nextStep(current, history) {
         if (current > number) {
             return null;
         }
         if (current === number) {
             return history;
-		}
+        }
         return executeStepsRandomlyAndPickOne(current, history);
     }
     return nextStep(1, `1`);
