1dadebd5c5323f26467ddf2961e1d133db25ef61
Git Faf <git.faf@gmail.com>
2017-09-06 12:13:31 +0530

    [JS] rotation - asynced

--- data-structures/javascript/arrays-one-dimensional/rotate.js
+++ data-structures/javascript/arrays-one-dimensional/rotate.js
@@ -16,8 +16,9 @@
  * rotateLeftOnce([]) === []
  * @param {Array} array - Array to be rotated.
  * @returns {Array} - Returns once left-rotated array 
+ * @async
  */
-function rotateLeftOnce(array) {
+async function rotateLeftOnce(array) {
     if (array && array.length > 1) {
         let tmp = array.shift();
         array.push(tmp);
@@ -31,8 +32,9 @@ function rotateLeftOnce(array) {
  * @example rotateRightOnce([]) === []
  * @param {Array} array - Array to be rotated.
  * @returns {Array} - Returns once right-rotated array 
+ * @async
  */
-function rotateRightOnce(array) {
+async function rotateRightOnce(array) {
     if (array && array.length > 1) {
         let tmp = array.pop();
         array.unshift(tmp);
@@ -44,18 +46,19 @@ function rotateRightOnce(array) {
  * rotate an array, n times, as per rotation logic.
  * @example rotate([1, 2, 3, 4, 5], 2, rotateLeftOnce); returns [3, 4, 5, 1, 2]
  * @example rotate([1, 2, 3, 4, 5], 2, rotateRightOnce); returns [4, 5, 1, 2, 3]
+ * @async
  * @param {Array} array - The array to rotate
  * @param {Number} times - Number of times it should be rotated.
  * @param {Function} rotation - Logic, how to rotate.
  * @returns {Array} {times} rotated.
  */
-function rotate(array, times, rotation) {
+async function rotate(array, times, rotation) {
     let len = array.length;
     // Remove extra rotations.
     times = times % len;
     // loop through single rotation <times> times
     for (let i = 0; i < times; i++) {
-        array = rotation(array);
+        array = await rotation(array);
     }
     return array;
 }
@@ -66,9 +69,10 @@ function rotate(array, times, rotation) {
  * @param {Array} array - Array to be rotated.
  * @param {Number} times - Number of times array should be rotated; default = 1
  * @returns {Array} - Returns times left-rotated array 
+ * @async
  */
-function rotateLeft(array, times = 1) {
-    return times > 0 && array.length > 1 ? rotate(array, times, rotateLeftOnce) : array;
+async function rotateLeft(array, times = 1) {
+    return times > 0 && array.length > 1 ? await rotate(array, times, rotateLeftOnce) : array;
 }
 
 /** PUBLIC fn
@@ -77,9 +81,10 @@ function rotateLeft(array, times = 1) {
  * @param {Array} array - Array to be rotated.
  * @param {Number} times - Number of times array should be rotated; default = 1
  * @returns {Array} - Returns times right-rotated array 
+ * @async
  */
-function rotateRight(array, times = 1) {
-    return times > 0 && array.length > 1 ? rotate(array, times, rotateRightOnce) : array;
+async function rotateRight(array, times = 1) {
+    return times > 0 && array.length > 1 ? await rotate(array, times, rotateRightOnce) : array;
 }
 
 module.exports = {
--- data-structures/javascript/arrays-one-dimensional/rotate.spec.js
+++ data-structures/javascript/arrays-one-dimensional/rotate.spec.js
@@ -3,92 +3,74 @@ let lib = require('./rotate');
 /* NOTE
  * Following will not work, different objects.
  * expect(actual).toBe(expected);
+ * AND
+ * for loop tests that 
+ * every element in actual
+ * is equal to 
+ * every element in expected.
  * */
 
 /* Edge case - Empty array rotation */
 describe('empty array rotations', () => {
-    it('rotateLeftOnce should return empty array', () => {
+    it('rotateLeftOnce should return empty array', async() => {
         const array = [];
-        const actual = lib.rotateLeftOnce(array);
+        const actual = await lib.rotateLeftOnce(array);
         const expected = [];
+
         expect(actual.length).toBe(expected.length);
         expect(actual[0]).toBe(undefined);
     });
-    it('rotateRightOnce should return empty array', () => {
+    it('rotateRightOnce should return empty array', async() => {
         const array = [];
-        const actual = lib.rotateRightOnce(array);
+        const actual = await lib.rotateRightOnce(array);
         const expected = [];
+
         expect(actual.length).toBe(expected.length);
         expect(actual[0]).toBe(undefined);
     });
 });
 
-describe('rotating arrays to the left', () => {  
+describe('rotating arrays to the left', () => {
     describe('rotating once', () => {
-        it('rotates array once successfully', () => {
+        it('rotates array once successfully', async() => {
             const array = [1, 2, 3, 4, 5];
-            const actual = lib.rotateLeftOnce(array);
+            const actual = await lib.rotateLeftOnce(array);
             const expected = [2, 3, 4, 5, 1];
-            expect(actual.length).toBe(expected.length);
 
+            expect(actual.length).toBe(expected.length);
             for (let i = 0; i < actual.length; i++) {
-                // This loop tests that 
-                // every element in actual
-                // is equal to 
-                // every element in expected.
                 expect(actual[i]).toBe(expected[i]);
             }
         });
-        it('rotates array twice successfully', () => {
+        it('rotates array twice successfully', async() => {
             const array = [1, 2, 3, 4, 5];
-            const actual = lib.rotateLeftOnce(lib.rotateLeftOnce(array));
+            const actual = await lib.rotateLeftOnce(await lib.rotateLeftOnce(array));
             const expected = [3, 4, 5, 1, 2];
-            /* This will not work, different objects.
-            expect(actual).toBe(expected); */
 
             expect(actual.length).toBe(expected.length);
-
             for (let i = 0; i < actual.length; i++) {
-                // This loop tests that 
-                // every element in actual
-                // is equal to 
-                // every element in expected.
                 expect(actual[i]).toBe(expected[i]);
             }
         });
     });
     describe('rotating with cardinal number', () => {
-        it('rotateLeft by default rotates once', () => {
+        it('rotateLeft by default rotates once', async() => {
             const array = [1, 2, 3, 4, 5];
-            const actual = lib.rotateLeft(array);
+            const actual = await lib.rotateLeft(array);
             const expected = [2, 3, 4, 5, 1];
-            /* This will not work, different objects.
-            expect(actual).toBe(expected); */
 
             expect(actual.length).toBe(expected.length);
-
             for (let i = 0; i < actual.length; i++) {
-                // This loop tests that 
-                // every element in actual
-                // is equal to 
-                // every element in expected.
                 expect(actual[i]).toBe(expected[i]);
             }
         });
-        it('rotates array thrice successfully', () => {
+        it('rotates array thrice successfully', async() => {
             const array = [1, 2, 3, 4, 5];
-            const actual = lib.rotateLeft(array, 3);
+            const actual = await lib.rotateLeft(array, 3);
             const expected = [4, 5, 1, 2, 3];
-            /* This will not work, different objects.
-            expect(actual).toBe(expected); */
 
             expect(actual.length).toBe(expected.length);
-
             for (let i = 0; i < actual.length; i++) {
-                // This loop tests that 
-                // every element in actual
-                // is equal to 
-                // every element in expected.
                 expect(actual[i]).toBe(expected[i]);
             }
         });
@@ -97,73 +79,45 @@ describe('rotating arrays to the left', () => {
 
 describe('rotating arrays to the right', () => {
     describe('rotating once', () => {
-        it('rotates array once successfully', () => {
+        it('rotates array once successfully', async() => {
             const array = [1, 2, 3, 4, 5];
-            const actual = lib.rotateRightOnce(array);
+            const actual = await lib.rotateRightOnce(array);
             const expected = [5, 1, 2, 3, 4];
-            /* This will not work, different objects.
-            expect(actual).toBe(expected); */
 
             expect(actual.length).toBe(expected.length);
-
             for (let i = 0; i < actual.length; i++) {
-                // This loop tests that 
-                // every element in actual
-                // is equal to 
-                // every element in expected.
                 expect(actual[i]).toBe(expected[i]);
             }
         });
-        it('rotates array twice successfully', () => {
+        it('rotates array twice successfully', async() => {
             const array = [1, 2, 3, 4, 5];
-            const actual = lib.rotateRightOnce(lib.rotateRightOnce(array));
+            const actual = await lib.rotateRightOnce(await lib.rotateRightOnce(array));
             const expected = [4, 5, 1, 2, 3];
-            /* This will not work, different objects.
-            expect(actual).toBe(expected); */
 
             expect(actual.length).toBe(expected.length);
-
             for (let i = 0; i < actual.length; i++) {
-                // This loop tests that 
-                // every element in actual
-                // is equal to 
-                // every element in expected.
                 expect(actual[i]).toBe(expected[i]);
             }
         });
     });
     describe('rotating with cardinal number', () => {
-        it('rotateRight by default rotates once', () => {
+        it('rotateRight by default rotates once', async() => {
             const array = [1, 2, 3, 4, 5];
-            const actual = lib.rotateRight(array);
+            const actual = await lib.rotateRight(array);
             const expected = [5, 1, 2, 3, 4];
-            /* This will not work, different objects.
-            expect(actual).toBe(expected); */
 
             expect(actual.length).toBe(expected.length);
-
             for (let i = 0; i < actual.length; i++) {
-                // This loop tests that 
-                // every element in actual
-                // is equal to 
-                // every element in expected.
                 expect(actual[i]).toBe(expected[i]);
             }
         });
-        it('rotates array thrice successfully', () => {
+        it('rotates array thrice successfully', async() => {
             const array = [1, 2, 3, 4, 5];
-            const actual = lib.rotateRight(array, 3);
+            const actual = await lib.rotateRight(array, 3);
             const expected = [3, 4, 5, 1, 2];
-            /* This will not work, different objects.
-            expect(actual).toBe(expected); */
 
             expect(actual.length).toBe(expected.length);
-
             for (let i = 0; i < actual.length; i++) {
-                // This loop tests that 
-                // every element in actual
-                // is equal to 
-                // every element in expected.
                 expect(actual[i]).toBe(expected[i]);
             }
         });
